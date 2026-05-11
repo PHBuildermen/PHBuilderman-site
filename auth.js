ee0b72c410e33637f4e14656775b86477870e61a
PHBuildermen <ArenasSibayan@gmail.com>
2026-05-11 22:27:25 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,28 @@
+// auth.js
+let currentUser = null;
 
+auth.onAuthStateChanged(user => {
+    currentUser = user;
+    if (user && window.location.pathname.includes('dashboard') && !isAdmin(user)) {
+        window.location.href = 'posts.html';
+    }
+});
+
+function isAdmin(user) {
+    return user && (user.email === "YOUR_EMAIL@gmail.com"); // ← Change to your Gmail
+}
+
+async function loginWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+        await auth.signInWithPopup(provider);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function logout() {
+    auth.signOut().then(() => {
+        window.location.href = 'index.html';
+    });
+}
