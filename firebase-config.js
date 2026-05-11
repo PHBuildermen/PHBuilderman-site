3e944956cb36227361cad3b78328a2f4f3ef180e
PHBuildermen <ArenasSibayan@gmail.com>
2026-05-11 22:43:32 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,12 @@
-
-// firebase-config.js
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "123456789",
+    apiKey: "YOUR_API_KEY_HERE",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "1234567890",
     appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
