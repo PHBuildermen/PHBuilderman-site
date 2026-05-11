bbf49fbd9c89ca33ae0ba42bb94324d09115f145
PHBuildermen <ArenasSibayan@gmail.com>
2026-05-11 23:08:50 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,23 @@
-// firebase-config.js
-const firebaseConfig = {
-  apiKey: "AIzaSyxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",   // ← PALITAN MO ITO
-  authDomain: "phbuilderman-site.firebaseapp.com", // ← PALITAN MO ITO
-  projectId: "phbuilderman-site",                  // ← PALITAN MO ITO
-  storageBucket: "phbuilderman-site.appspot.com",
-  messagingSenderId: "123456789012",               // ← PALITAN MO ITO
-  appId: "1:123456789012:web:xxxxxxxxxxxxxxxxxxxxxxxx" // ← PALITAN MO ITO
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC61bm4frpqqVwqYiK87FARe78lzH88Hng",
+    authDomain: "phbuilderman-site.firebaseapp.com",
+    projectId: "phbuilderman-site",
+    storageBucket: "phbuilderman-site.firebasestorage.app",
+    messagingSenderId: "777840097655",
+    appId: "1:777840097655:web:eccaefaf541d617e794972",
+    measurementId: "G-4NKNN8ZFKV"
+  };
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
